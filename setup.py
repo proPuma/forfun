105f9f8f32c8c95b6413383af5a179019336e241
proPuma <103031461+proPuma@users.noreply.github.com>
2022-06-07 22:58:15 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os, sys
 import banner
 import password 
-os.system("termux-open-url https://t.me/noname_pxpxpx")
+os.system("termux-open-url https://t.me/who_am_i_xxx")
 print(banner)
 print(password)
